e38b6ede0526c7e274752d9a597e09f740a99506
Meluleki Dube <mellarmellah@gmail.com>
2019-10-12 19:10:18 +0200

    added models

--- medical_system/doctor/models.py
+++ medical_system/doctor/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from patient import Patient
 
 # Create your models here.
+class Doctor(models.Model):
+    #doctor personal data
+    licence_number  = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=100)
+    second_name = models.CharField(max_length=100)
+    
+    location = models.CharField(max_length = 100)
+    phone_number = models.CharField(blank=False, null=False)
+    email_address = models.CharField(max_length=100)
+    
+    medical_scheme = models.CharField(max_length=100)
+    speciality = models.CharField(max_length=100)
+  
+class Appointment(models.Model):
+    
+    time = models.DateTimeField()
+    
+    doctor  = models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient,on_delete=models.CASCADE)
+    
--- medical_system/patient/models.py
+++ medical_system/patient/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length = 100)
+    surname = models.CharField(max_length = 100)
+    
+    email_address = models.CharField(max_length = 100)
+    phone_number = models.CharField(max_length = 100)
+    
+    blood_type = models.CharField(max_length= 2)
+    
+class PatientRecord(models.Model):
+    #symptonms, diagnosis, prescription
+    symptoms = models.CharField(max_length = 1000)
+    diagosis  = models.CharField(max_length = 1000)
+    prescription = models.CharField(max_length = 1000)
+    
